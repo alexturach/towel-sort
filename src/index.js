efb848bafea8d73906ea9ca9369ece3a117d21b8
alexturach <turach.ad@gmail.com>
2022-08-10 16:53:55 +0300

    init: update js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports =  
+function towelSort (matrix=[]) {
+    let i;    
+    if (matrix.length === 0) {
+      return [];
+
+    }
+    for (i = 1; i < matrix.length; i+=2) {
+      matrix[i].sort((a, b) => b - a);
+    }
+    let result = [];
+    let arr = (matrix.toString()).split(',');
+  
+    for (i = 0; i < arr.length; i++) {
+      result.push(parseInt(arr[i]));
+    }
+        return result;
+  }
